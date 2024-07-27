eb03641ed8a89e7df6ef425fce878d8062ce7731
Narsa011 <156831315+Narsa011@users.noreply.github.com>
2024-07-27 03:24:54 +0000

    variables type

---
+++ 01-basic/01-variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "Swroopsingh123@google.com"
+var accountPassword = "12354"
+accountCity = "Sanchore"
+accountPassword = "123456789000"
+accountstate = "Rajasthan"
+accountUser = "NarendraSingh" 
+accountUserAge = "18Yr"
+accountUserDob = "11/01/2004"
+accountUserMobileNumber = "6376151185"
+// value nhi dalne per error ahega 
+
+// var ka use nhi kare ya sahi nhi h 
+
+
+
+
+console.table({accountPassword,accountEmail,accountId,accountCity,accountUser,accountUserAge,accountUserDob,accountUserMobileNumber,});
