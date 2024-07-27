8e315b5fb9fe3140c994678cc8ee05c41a9e0881
Narsa011 <156831315+Narsa011@users.noreply.github.com>
2024-07-27 05:55:50 +0000

    conversion of data types

---
+++ 01-basic/03-conversionopreation.js
@@ -0,0 +1,29 @@
+// let score = "3abc3";
+
+// console.log(typeof (score));
+
+// let valueInNumber = Number (score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// Notes ------------------------
+// "33" => 33
+// "33abc" =>Nan (Not an number)
+//true => 1; false => 0;
+
+// let isLoggedIn = "Narendra" 
+// let booleanisLoogedIn = Boolean (isLoggedIn);
+//  console.log(booleanisLoogedIn);
+
+ // Notes -----------------
+ // "narendra" => true
+ // "" => false
+ // null => false 
+// 4555 => trur
+// 0 => false
+// 1 => true
+
+let somenumber = 33
+let stringNumber = String (somenumber);
+ console.log(stringNumber);
+ console.log( typeof stringNumber);
